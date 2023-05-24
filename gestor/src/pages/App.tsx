19e5d855965d2a9b959960e78f31fb853a682844
IsadoraMello <isadora.mello@unifebe.edu.br>
2023-05-24 12:08:08 -0300

    create project react

---
+++ gestor/src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <header className="App-header">
+       teste
+      </header>
+    </div>
+  );
+}
+
+export default App;
